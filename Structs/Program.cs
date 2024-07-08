a7c04d4fd4832ce57b3c49ed1b1f6290b43d5bfd
Karim <karim@karim>
2024-07-08 15:40:31 +0300

    Create Structs Project

---
+++ Structs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Structs
+{
+    // Structs are old data types
+    // From the days of structural programming paradigm C++
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
