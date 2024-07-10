a707dc7034bad7cca948a32d370bdc5674e512d2
Karim <karim@karim>
2024-07-10 22:23:41 +0300

    Encapsulation Part I

---
+++ Encapsulation/Employee.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    // Without Encapsulation => ID Attribute
+    internal struct Employee
+    {
+        // How to do Encapsulation
+        // 1. Private Attributes
+        // 2. [Getter setter] - [Properties]
+        // 3. Property ("Full", "Automatic", "Indexer")
+
+        // Attributes
+        public int ID;
+        private string? name; // Private attributes in lower case
+        private decimal salary; // Private attributes in lower case
+
+        #region Getter Setter
+        // Getter Setter
+        public string? GetName()
+        {
+            return name;
+        }
+
+        // if i delete the setter, nobody can change the name after creating it
+        public void SetName(string? _name)
+        {
+            name = _name?.Length > 5 ? _name.Substring(0, 5) : _name;
+            // if longer than 5 characters take the first 5 only
+            // Now we can control the data entered
+        }
+        #endregion
+
+        #region Property
+        // The easier way
+        // The recommended way
+        // Outside Class use the Property
+        // Inside the class use the Attribute itself "its own scope"
+
+        // propfull + TAB Twice for a full property
+        // prop + TAB for an auto property
+
+        // This is a full Property "Not common"
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set // if changed to "private set" no one will be able to modify it
+            {
+                salary = value < 3000? 3000:value;
+                // value is a reserved keyword
+            }
+        }
+
+        public int Age { get; set; } // This is a Property not an Attribute
+        // The compiler will generate a Hidden Private Attribute automatically "Backing Field"
+
+        public decimal Deduction // A Property not an Attribute
+        {
+            get { return salary * 0.2M; }
+        }
+        #endregion
+
+        // Constructors (Rt click + quick actions)
+        public Employee(int id, string? userName, decimal salary, int age)
+        {
+            ID = id;
+            name = userName;
+            this.salary = salary;
+            this.Age = age;
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            return $"ID = {ID}\nName = {name}\nSalary = {salary:c}\nAge = {Age}";
+        }
+
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Encapsulation
+{
+    // The 4 pillars of OOP
+    // 1. Encapsulation
+    // 2. Inheritance
+    // 3. Polymorphism
+    // 4. Abstraction
+
+    // Encapsulation means separation of data definition and data use
+    // Inheritance to decrease the amount of repeated code
+    // Polymorphism (Overloading and Overriding)
+    // Abstraction (Hide the details of the business logic)
+
+    // 3 problems before Encapsulation
+    // 1. Direct access of the attributes
+    // 2. I can't make read only attributes
+    // 3. I can't put limits on the attributes
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee01 = new Employee(10, "Karim", 2500, 40);
+            Console.WriteLine(Employee01);
+
+            Employee01.ID = 20; // Direct access of the attribute
+            // If I change ID to EmpID for example, I'll need to change it everywhere in the program
+            // I can change the ID at anytime
+
+            Employee01.SetName("Fayez");
+            Console.WriteLine(Employee01.GetName());
+            // chnaging the attribute name won't affect the code here
+            // since it's accessed through the getter setter
+
+            Employee01.Salary = 3500;
+            Console.WriteLine(Employee01.Salary);
+            // Access through property
+        }
+    }
+}
