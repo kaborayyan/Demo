86cd57969d0f15a424758504e575f59c2771ecb0
Karim <karim@karim>
2024-07-11 21:48:04 +0300

    Create Inheritance Project

---
+++ Classes/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Car
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+
+        public override string ToString()
+        {
+            return $"Car Id: {Id}\nModel Name: {Model}\nCar Speed: {Speed}";
+        }
+        #endregion
+        
+        #region Constructors        
+        public Car(int id, string model, double speed)
+        {
+            Id = id;
+            Model = model;
+            Speed = speed;
+        }
+
+        // Constructor Overloading
+        // Must be different in numbers of passed parameters or
+        // their order or type of parameters
+        // Constructor chaining is used to avoid repeating the code
+
+        //public Car(int id, string model) // Constructor OverLoading
+        //{
+        //    Id = id;
+        //    Model = model;
+        //    Speed = 200;
+        //}
+        
+        public Car(int id, string model):this(id, model,200) // Constructor Chaining
+        {            
+        }
+
+        //public Car(int id) // Constructor Overloading
+        //{
+        //    Id = id;
+        //    Model = "Toyota";
+        //    Speed = 200;
+        //}
+
+        public Car(int id):this(id,"Toyota") // Constructor Chaining
+        {            
+        }
+
+        #endregion
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Car Car01;
+            // Create reference in stack, allocate 4 bytes for the reference
+            // but 0 bytes in heap
+
+            // Car01 = new Car();
+            // 1. Allocate memory at heap ~ 16 bytes
+            // 2. Allocate default values in those bytes
+            // 3. Call user defined Constructors if present, if not present the compiler will generate it
+            // 4. Assign the reference car01 to allocated object
+            // Console.WriteLine(Car01); // Will print default values
+
+            Car Car01 = new Car(10, "BMW", 250);
+            Console.WriteLine(Car01);
+
+            Car Car02 = new Car(20, "Nissan");
+            Console.WriteLine(Car02);
+
+            Car Car03 = new Car(30);
+            Console.WriteLine(Car03);
         }
     }
 }
