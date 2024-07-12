f4c0b61e7acd4c2e6d20f1e72f6e4766eeb44001
Karim <karim@karim>
2024-07-12 12:47:14 +0300

    Finished Nullable Types and Null Propagation Operator

---
+++ NullableTypes/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public Department Department { get; set; }
+    }
+
+    class Department
+    {
+        public string DeptName { get; set; }
+    }
+}
--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -4,7 +4,99 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Value Types
+            // To allow null values add? after the data type
+            int X = 10; // does not allow null
+            int? Y = null; // allows null
+            Y = X; // Valid but the opposite is invalid
+
+            Nullable<int> Z; // This was the old way of doing it, not used anymore
+            Z = (int?)X; // Old way since it's considered casting, do not use
+
+            int? A = null;
+            int B = (int)A; // Explicit Casting Unsafe
+
+            // How to do it safely?
+            int C;
+
+            //if (A != null)
+            //{
+            //    C = (int)A;
+            //}
+            //else
+            //{
+            //    C = 0;
+            //}
+
+            C = A != null ? (int)A : 0; // same as above
+
+            //if (A.HasValue)
+            //{
+            //    C = (int)A;
+            //}
+            //else
+            //{
+            //    C = A.Value;
+            //}
+
+            C = A.HasValue ? A.Value : 0; // Safe Casting, no warning 
+            #endregion
+
+            #region Reference Types
+            // Null is the default value for reference types
+            string NotNullable = default; // null
+            NotNullable = null;
+            // Both accepted but with warning because you allocated memory for a null value
+
+            NotNullable = null!; // Null forgiveness operator
+
+            string? Nullable = null; // the correct way to do it
+
+            // Remember Null is not a problem in reference data types
+            NotNullable = Nullable; // Possible here but was not possible in Value types 
+
+            Console.WriteLine(Nullable);
+            Console.WriteLine(NotNullable);
+            #endregion
+
+            #region Null Propagation Operator
+            int[] Numbers = null; // Null Reference Exception
+
+            // To Avoid NullReferenceException
+            // The first long way
+            for (int i = 0; (Numbers != null) && (i < Numbers.Length); i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+
+            // Another long way but more efficient
+            // It won't start looping if the if condition is false
+            if (Numbers != null)
+            {
+                for (int i = 0; i < Numbers.Length; i++)
+                {
+                    Console.WriteLine(Numbers[i]);
+                }
+            }
+
+            // The shorter way but less efficient
+            // It will start the loop
+            for (int i = 0; i < Numbers?.Length; i++) // add ? to the Array name
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+
+            //Null coalescing operator
+            int Length = Numbers?.Length ?? 0;
+
+            // We use Null Propagation operator and Null Coalescing operator when dealing with database
+            Employee Employee01 = new Employee();
+            Employee01.Name = "Karim";
+            Employee01.Department.DeptName = "IT";
+
+            // We use them to avoid null exception
+            Console.WriteLine(Employee01?.Department?.DeptName ?? "Department Not Found"); // Safe
+            #endregion
         }
     }
 }
