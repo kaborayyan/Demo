0dcd560d32ec4d018bd74df076e5e480a4af9ba4
Karim <karim@karim>
2024-07-17 15:54:15 +0300

    Creating Project Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Interface
+{
+    // Interfaces are Reference - Non Primitive - User Defined - Data Type
+    // Just Like Classes
+    // They are a Contract between two developers
+    // One wrote it and the other will implement it
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
