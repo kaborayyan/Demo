985dca98159d51d7071ca453635c2e1d375f6464
kaborayyan <104945426+kaborayyan@users.noreply.github.com>
2024-09-08 19:03:55 +0300

    Delete Company.NET8 directory

--- Company.NET8/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Company.NET8.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
